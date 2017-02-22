4fbb383f45775f0ad946148d9d7b9ee3e69474cf
Alfonso Olavarria <alfonsojn15@gmail.com>
2017-02-22 16:41:00 -0400

    Update enviro.js

--- config/enviro.js
+++ config/enviro.js
@@ -1,9 +1,9 @@
 
 
 module.exports = {
-  'PARSE_APP_ID': 've.com.turnos',
+  'PARSE_APP_ID': 'appid',
   'PARSE_SERVER_URL' :  'http://localhost:1339/parse',
-  'PARSE_MASTER_KEY' : '12345devkey',
+  'PARSE_MASTER_KEY' : 'masterkey',
   'PORT' : 1339,
   'PARSE_MOUNT':'/parse',
   'DATABASE_URI':'mongodb://localhost:27017/dev'
